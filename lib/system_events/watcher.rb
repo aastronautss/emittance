c836e22a974151924a72b11bdd6b2944f0b7e02c
Tyler Guillen <tyler.guillen@touchofmodern.com>
2017-08-23 11:41:48 -0700

    watcher spec

--- lib/system_events/watcher.rb
+++ lib/system_events/watcher.rb
@@ -1,7 +1,7 @@
 module SystemEvents
   module Watcher
     def watch(identifier, &callback)
-      SystemEvents::Broker.register identifier, callback
+      SystemEvents::Broker.register identifier, &callback
     end
   end
 end
