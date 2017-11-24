0912ef6ecccf986888e79a91275b3416d8129c9a
Tyler Guillen <tyguillen@gmail.com>
2017-11-24 13:20:23 -0800

    namespacing

--- lib/system_events/emitter.rb
+++ lib/system_events/emitter.rb
@@ -1,58 +1,56 @@
-module SystemEvents
-  module Emitter
-    class << self
-      def extended(extender)
-        SystemEvents::Emitter.emitter_eval(extender) do
-          include ClassAndInstanceMethods
-          extend ClassAndInstanceMethods
-        end
-      end
-
-      def non_emitting_method_for(method_name)
-        "_non_emitting_#{method_name}".to_sym 
-      end
-
-      def emitting_method_event_name(emitter, method_name)
-        "#{emitter}##{method_name}"
-      end
-
-      def emitter_eval(klass, *args, &blk)
-        if klass.respond_to? :class_eval
-          klass.class_eval *args, &blk
-        else
-          klass.singleton_class.class_eval(*args, &blk)
-        end
+module SystemEvents::Emitter
+  class << self
+    def extended(extender)
+      SystemEvents::Emitter.emitter_eval(extender) do
+        include ClassAndInstanceMethods
+        extend ClassAndInstanceMethods
       end
     end
 
-    module ClassAndInstanceMethods
-      def emit(identifier, *payload)
-        now = Time.now
-        SystemEvents::Broker.process_event identifier, now, self, payload
-      end
+    def non_emitting_method_for(method_name)
+      "_non_emitting_#{method_name}".to_sym 
     end
 
-    def emits_on(*method_names)
-      method_names.each do |method_name|
-        non_emitting_method = SystemEvents::Emitter.non_emitting_method_for method_name
+    def emitting_method_event_name(emitter, method_name)
+      "#{emitter}##{method_name}"
+    end
 
-        SystemEvents::Emitter.emitter_eval(self) do
-          if method_defined?(non_emitting_method)
-            warn "Already emitting on #{method_name}"
-            return
+    def emitter_eval(klass, *args, &blk)
+      if klass.respond_to? :class_eval
+        klass.class_eval *args, &blk
+      else
+        klass.singleton_class.class_eval(*args, &blk)
+      end
+    end
+  end
+
+  module ClassAndInstanceMethods
+    def emit(identifier, *payload)
+      now = Time.now
+      SystemEvents::Broker.process_event identifier, now, self, payload
+    end
+  end
+
+  def emits_on(*method_names)
+    method_names.each do |method_name|
+      non_emitting_method = SystemEvents::Emitter.non_emitting_method_for method_name
+
+      SystemEvents::Emitter.emitter_eval(self) do
+        if method_defined?(non_emitting_method)
+          warn "Already emitting on #{method_name}"
+          return
+        end
+
+        alias_method non_emitting_method, method_name
+
+        module_eval <<-EOS, __FILE__, __LINE__ + 1
+          def #{method_name}(*args, &blk)
+            event_name = SystemEvents::Emitter.emitting_method_event_name(self.class, __method__)
+            return_value = #{non_emitting_method}(*args, &blk)
+            emit event_name, return_value
+            return_value
           end
-
-          alias_method non_emitting_method, method_name
-
-          module_eval <<-EOS, __FILE__, __LINE__ + 1
-            def #{method_name}(*args, &blk)
-              event_name = SystemEvents::Emitter.emitting_method_event_name(self.class, __method__)
-              return_value = #{non_emitting_method}(*args, &blk)
-              emit event_name, return_value
-              return_value
-            end
-          EOS
-        end
+        EOS
       end
     end
   end
