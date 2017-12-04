b7eb6586022e065bb0bef917b667c9fd5381c16a
Tyler Guillen <tyguillen@gmail.com>
2017-12-04 15:44:38 -0800

    action documentation

--- lib/emittance/action.rb
+++ lib/emittance/action.rb
@@ -1,13 +1,13 @@
 ##
-# There are certain classes (ergo objects) that represent an action taken by another object. This pattern goes like so:
+# Consider the usual "Service Object" pattern:
 #
 #   class Foo
 #     def assign
-#       Assignment.new(self).call
+#       FooAssignment.new(self).call
 #     end
 #   end
 #
-#   class Assignment
+#   class FooAssignment
 #     attr_reader :assignable
 #
 #     def initialize(assignable)
@@ -21,14 +21,11 @@
 #     # ...
 #   end
 #
-# This pattern is useful for maintaining the single responsibility principle, delegating complex tasks to other objects
-# even when (in this particular case), it might be sensible for the +assign+ message to be sent to +Foo+. This has
-# numerous benefits, including the ability for actions like +Assignment+ to take a duck type.
+# There are variations on this pattern, the idea is that the service object represents something that your application
+# is doing. However, this can easily just become a proxy for the same antipattern it was made to solve. We might wind
+# up with a +#call+ method like the following:
 #
-# However, this can easily just become a proxy for the same antipattern it was made to solve. We might wind up with a
-# +#call+ method like the following:
-#
-#   class Assignment
+#   class FooAssignment
 #     # ...
 #
 #     def call
@@ -41,44 +38,14 @@
 #     # ...
 #   end
 #
-# +Assignment+ is suddenly collaborating with a whole bunch of objects! This isn't bad in itself, but it might cause
-# some problems further on down the road as we add more responsibilities to +Assignment+. Do we really want all these
-# things to happen every time an +Assignment+ happens? If not, assuming this pattern we'll have to add a bunch of
-# control flow:
-#
-#   class Assignment
-#     # ...
-#
-#     def call
-#       do_stuff
-#
-#       if some_condition
-#         do_stuff_to_another_object
-#
-#         if some_other_condition
-#           do_stuff_to_something_else
-#         else
-#           do_stuff_to_yet_another_thing
-#         end
-#       elsif yet_another_condition
-#         do_other_stuff_to_that_other_object
-#       else
-#         dont_actually_do_anything_but_notify_someone
-#       end
-#     end
-#
-#     # ...
-#   end
-#
-# This is obviously an extreme example (but not unheard of!), but it gets to the core of what this module tries to
-# solve. +Emittance::Action+ helps facilitate the single responsibility principle by emitting an event whenever we
-# invoke +#call+ on an object like +Assignment+.
+# Mixing in +Emittance::Action+ allows us to keep the expressitivity that a Service Object is made to provide, while
+# preventing us from having to give such an object too many responsibilities.
 #
 # == Usage
 #
 # First, define a class and include this module:
 #
-#   class Assignment
+#   class FooAssignment
 #     include Emittance::Action
 #
 #     attr_reader :assignable
@@ -88,13 +55,9 @@
 #     end
 #   end
 #
-# Per the pattern explained above, instances of this class are representations of an action being carried out. This
-# class should have a very minimal interface (maybe, at most, some getter methods for its instance variables so the
-# handler can make decisions based on its state).
-#
 # Next, we'll implement the +#call+ instance method. +Emittance::Action+ will take care of the dirty work for us:
 #
-#   class Assignment
+#   class FooAssignment
 #     # ...
 #
 #     def call
@@ -104,16 +67,16 @@
 #     # ...
 #   end
 #
-# Again, this method should do a single thing. From here, your code should be able to run without error! You might
-# notice, though, that a mysterious class will have been defined after loading this file.
+# From here, your code should be able to run without error! You might notice, though, that a mysterious class will have
+# been defined after loading this file.
 #
-#   defined? AssignmentHandler
+#   defined? FooAssignmentHandler
 #   => "constant"
 #
 # Next, we can open up this class to implement the event handler. +Emittance+ will look for a method called
-# +#handle_call+, and invoke it whenever, in this example, +Assignment#call+ is called.
+# +#handle_call+, and invoke it whenever, in this example, +FooAssignment#call+ is called.
 #
-#   class AssignmentHandler
+#   class FooAssignmentHandler
 #     def handle_call
 #       notify_someone(action)
 #     end
@@ -125,7 +88,7 @@
 # will allow us to access its data and make decisions based on it.
 #
 # Now, this seems like we're passing the buck of all that control flow to yet another object, but this pattern has
-# several advantages. First, we can disable +Emittance+ at will, so if we ever want to shut +Assignment+ actions
+# several advantages. First, we can disable +Emittance+ at will, so if we ever want to shut +FooAssignment+ actions
 # off from their listeners, that is always an option to us. Second, to address the concern raised at the beginning of
 # this paragraph, this paradigm puts us into the mindset of spreading the flow of our program out across multiple
 # action/handler pairs, allowing us to think more clearly about what our code is doing.
