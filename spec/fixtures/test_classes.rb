24cb8839c8fc070ec8fc62938a255a942d197275
Tyler Guillen <tyguillen@gmail.com>
2017-12-09 17:12:05 -0800

    clean up event_lookup

--- spec/fixtures/test_classes.rb
+++ spec/fixtures/test_classes.rb
@@ -1,10 +1,16 @@
 module Emittance
   module SpecFixtures
+    class Foo; end
+    class Bar; end
+    class FooBar; end
+
     class FooEmitter
       extend Emittance::Emitter
 
       def emit_foo
         emit :foo
+
+        'return value'
       end
 
       def do_something
@@ -43,3 +49,16 @@ module Emittance
     end
   end
 end
+
+# Top-level namespace classes
+
+class Foo; end
+class Bar; end
+class FooBar; end
+class Foo::Baz; end
+
+class FooEvent < Emittance::Event; end
+class BarEvent < Emittance::Event; end
+class FooBarEvent < Emittance::Event; end
+class FooBarFooEvent < Emittance::Event; end
+class Foo::BazEvent < Emittance::Event; end
