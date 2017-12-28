9177a2811b97fbaca193d3f4f5c1585813045bab
Tyler Guillen <tyguillen@gmail.com>
2017-12-28 14:25:39 -0800

    documentation

--- lib/emittance/dispatcher.rb
+++ lib/emittance/dispatcher.rb
@@ -17,30 +17,40 @@ module Emittance
   #
   class Dispatcher
     class << self
+      # @param subklass [Class] the new subclass
       def inherited(subklass)
         subklass.instance_variable_set '@registrations', RegistrationMap.new
       end
 
+      # Calls the subclass's +_process_event+ method.
       def process_event(event)
         _process_event(event)
       end
 
+      # Calls the subclass's +_register+ method.
       def register(identifier, &callback)
         _register(identifier, &callback)
       end
 
+      # Calls the subclass's +_register_method_call+ method.
       def register_method_call(identifier, object, method_name)
         _register_method_call(identifier, object, method_name)
       end
 
+      # @param identifier the identifier the registrations for which you would like to look up
+      # @return [RegistrationCollectionProxy] an enumerable containing all registrations for a given identifier
       def registrations_for(identifier)
         registrations[identifier]
       end
 
+      # @return [RegistrationMap] the registrations
       def clear_registrations!
         registrations.each_key { |key| clear_registrations_for! key }
+        registrations
       end
 
+      # @param identifier the identifier the registrations for hwich you would like to clear
+      # @return [RegistrationCollectionProxy] the cleared registration proxy
       def clear_registrations_for!(identifier)
         registrations_for(identifier).clear
       end
@@ -69,23 +79,31 @@ module Emittance
       SPECIAL_IDENTIFIER_REGEX = /^\@/
 
       class << self
-        # @param
+        # @param identifier the identifier we want to know information about
+        # @return [Boolean] true if the identifier is a special one, false otherwise
         def special_identifier?(identifier)
           identifier.to_s =~ SPECIAL_IDENTIFIER_REGEX
         end
       end
 
+      # Build a registration map.
       def initialize
         @reg_map = {}
       end
 
+      # @param identifier the identifier you wish to lookup registrations for
+      # @return [RegistrationCollectionProxy] a collection of registrations for that identifier
       def [](identifier)
         lookup_term, keys = keys_for(identifier)
         collection_for(lookup_term, keys)
       end
 
+      # @param args args passed to +Hash#each_key+
+      # @param blk block passed to +Hash#each_key+
+      # @return [RegistrationMap] self
       def each_key(*args, &blk)
         reg_map.each_key(*args, &blk)
+        self
       end
 
       private
@@ -138,14 +156,20 @@ module Emittance
     # A collection proxy for registrations. Can include multiple key/value pairs.
     #
     class RegistrationCollectionProxy
+      # @param lookup_term the term initially used to lookup the registrations
+      # @param mappings [Hash] the mappings of identifiers to their respective registrations
       def initialize(lookup_term, mappings)
         @lookup_term = lookup_term
         @mappings = mappings
       end
 
+      # @param args args passed to +Array#each+
+      # @param blk block passed to +Array#each+
+      # @return [RegistrationCollectionProxy] self
       def each(*args, &blk)
         arrays = mappings.values.map(&:to_a)
         arrays.flatten.each(*args, &blk)
+        self
       end
 
       def empty?
@@ -154,10 +178,12 @@ module Emittance
 
       def <<(item)
         mappings[lookup_term] << item
+        self
       end
 
       def clear
         mappings.values.each(&:clear)
+        self
       end
 
       private
