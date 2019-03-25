e3663c53ac13d45303e915734efb273ec6e91b90
Tyler Guillen <tyguillen@gmail.com>
2019-03-25 15:31:12 -0700

    flexible event class retrieval for topic lookup (#24)
    * flexible event class retrieval for topic lookup
    
    * fix failing specs
    
    * remove ruby 2.5 from travis for now

--- lib/emittance/topic_lookup.rb
+++ lib/emittance/topic_lookup.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
 module Emittance
+  class Event
+  end
+end
+
+module Emittance
   ##
   # Since events don't need to be dynamically generated when using topics, we essentially want to stub out all of the
   # class creation and registration logic.
   #
   module TopicLookup
+    DEFAULT_EVENT_KLASS = Emittance::Event
+
     class << self
+      attr_writer :event_klass
+
       def identifiers_for_klass(_klass, event = nil)
         raise ArgumentError, 'Cannot generate identifiers without an event' unless event
 
@@ -18,7 +27,13 @@ module Emittance
       end
 
       def find_event_klass(*_identifiers)
-        Emittance::Event
+        event_klass
+      end
+
+      private
+
+      def event_klass
+        @event_klass ||= DEFAULT_EVENT_KLASS
       end
     end
   end
